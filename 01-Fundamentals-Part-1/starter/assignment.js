53a9c5b61bde5ea105934379758b41495d69f454
JimWNE <52102536+jimWNE@users.noreply.github.com>
2020-12-11 15:15:26 +0200

    Lecture 12

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,20 @@
+// Assignment JavaScript Fundamentals - Part 1 :
+
+// Values and Variables
+let country = 'Finland';
+let continent = 'Europe';
+let population = 1124508;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// Data Types
+let isIsland = true
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);

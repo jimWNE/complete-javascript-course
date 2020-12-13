c7508f86a4d3b0d35b51eea14744b658ddc6c439
JimWNE <52102536+jimWNE@users.noreply.github.com>
2020-12-13 23:27:27 +0200

    More studies

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,6 +1,7 @@
 // Assignment JavaScript Fundamentals - Part 1 :
-
+/*
 // Values and Variables
+console.log("VALUES AND VARIABLES");
 let country = 'Finland';
 let continent = 'Europe';
 let population = 1124508;
@@ -9,8 +10,8 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-
 // Data Types
+console.log("DATA TYPES");
 let isIsland = true
 let language;
 
@@ -18,3 +19,44 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+// Let, const and var
+console.log("LET,CONST AND VAR");
+
+const language = "Finnish";
+const country = "Finland";
+const continent = "Europe";
+
+country = "Russia"
+
+
+// Basic Operators
+console.log("BASIC OPERATORS");
+
+let population = 1124508;
+console.log(population);
+population /= 2;
+console.log(population);
+population++;
+console.log(population);
+
+const populationFI = 1124508;
+const populationSA = 6000000;
+const isMore = populationFI > populationSA;
+console.log(isMore);
+
+const averageCountryPopulation = 33000000;
+const isLessThanAverage = populationFI < averageCountryPopulation;
+console.log(isLessThanAverage);
+
+const portugalPopulation = 11
+const description = "Portugal is in Europe, and its"+' '+portugalPopulation+' '+"million people speak portuguese";
+console.log(description);
+
+// Or you can do it like this:
+console.log("Or you can do it like this:");
+const descriptionFirst = 'Portugal is in Europe,';
+const descriptionSecond = 'and its 11 million people speak portuguese.';
+const description2 = descriptionFirst + ' ' + descriptionSecond;
+console.log(description2);
+*/
